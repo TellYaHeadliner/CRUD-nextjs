5ba23bbb0e01804f10e679cec97b76b992588b07
TellYaHeadliner <buibaolam0302@gmail.com>
2024-11-01 07:18:40 +0700

    Demo changging

--- src/components/App.Footer.jsx
+++ src/components/App.Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <footer className="bg-gray-800 text-center py-4">
             <p className="text-sm text-gray-400">
-                &copy; {new Date().getFullYear()} Copyright by TellYaHeadliner
+                &copy; {new Date().getFullYear()} Copyright by Bùi Bảo Lâm
             </p>
         </footer>
     );
